82fb55869579db33a7613dbb7054e91ebc826fd1
Utente <everton_ravanelli@hotmail.com>
2024-10-20 11:40:40 +0200

    Mostrar mensagem preencha todos os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -24,13 +25,28 @@ class MainActivity : AppCompatActivity() {
         val edtcalcular = findViewById<Button>(R.id.btn_calcular)
 
         edtcalcular.setOnClickListener{
-            val altura: Float = edtaltura.text.toString().toFloat()
-            val peso: Float = edtpeso.text.toString().toFloat()
+            val alturaStr: String = edtaltura.text.toString()
+            val pesoStr: String = edtpeso.text.toString()
+
+            if (pesoStr == "" || alturaStr == ""){
+            // Mostrar mensagem para o usuario
+            Snackbar
+            .make(
+            edtpeso,
+            "Preencha todos os campos",
+            Snackbar.LENGTH_LONG
+              )
+              .show()
+
+            } else{
+
+            val peso = pesoStr.toFloat()
+            val altura = alturaStr.toFloat()
 
             val alturaQ2 = altura * altura
             val resultado = peso / alturaQ2
-            println("acao do botao" + resultado)
-
+            println(" acao do botao " + resultado)
+            }
 
         }
     }
